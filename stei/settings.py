4583dc78f488c0ca72301cc4a270c8c8edad0941
arsaizdihar <arsadihar@gmail.como>
2021-11-06 07:43:35 +0700

    update charset

--- stei/settings.py
+++ stei/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
             "PASSWORD": os.environ.get("DB_PASSWORD"),
             "HOST": "localhost",
             "PORT": "3306",
+            "OPTIONS": {"charset": "utf8mb4"},
         },
-        "OPTIONS": {"charset": "utf8mb4"},
     }
 
 
