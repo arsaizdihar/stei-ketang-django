1eec3b05b1524c6f6e37e64030d5ebbe74bc3306
arsaizdihar <arsadihar@gmail.como>
2021-11-17 19:41:05 +0700

    update admin site

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register((Candidate, Vote, Detail))
+
+@admin.display(description='Email')
+def email(obj):
+    return obj.user.email
+
+@admin.display(description='Number')
+def number(obj):
+    return obj.candidate.number
+
+@admin.display(description='Name')
+def name(obj):
+    return obj.candidate.name
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = (email, number, name)
+    list_filter = ("candidate__name", )
+    
+admin.site.register(Vote, VoteAdmin)
+admin.site.register((Candidate, Detail))
